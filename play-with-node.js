17ec8981f0b644de105b2c056d91595a13b3644c
Dhanushka_Sasanka <dhanushkasasankapayza@gmal.com>
2021-02-05 15:06:28 +0530

    file system in node

---
+++ play-with-node.js
@@ -0,0 +1,10 @@
+
+// accessing file System 
+const fileSystem = require('fs');
+
+
+
+fileSystem.writeFileSync('hello_Node.txt', "This content Written By Mr.Dhanushka with Node JS File System..");
+
+
+
