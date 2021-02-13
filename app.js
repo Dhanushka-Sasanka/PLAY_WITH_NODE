9d2b94e2cc1807281856a1d1819282aa5e552264
Dhanushka_Sasanka <dhanushkasasankapayza@gmal.com>
2021-02-13 13:08:48 +0530

    request parser with body-parser

--- app.js
+++ app.js
@@ -3,18 +3,35 @@ const experss = require('express');
 
 const app = experss();
 
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extends:false}));
 
-app.use('/add-customer',(req , res , next) =>{
+app.use('/add-customer', (req, res, next) => {
     console.log("route 2");
-    res.send("<html><head><body><h1>I AM NODE JS SERVER WITH EXPRESS JS...</h2></body></head></html>")
-    
+    res.send(`<html>
+        <head><body>
+                <form action="/customer" method="POST">
+                    <input type="text" name="product" />
+                    <button type="submit">ADD CUSTOMER</button>
+                </form>
+            </body>
+        </head>
+    </html>`);
+
 });
 
-app.use('/',(req , res , next) =>{
+app.use('/customer', (req, res, next) => {
+    console.log(req.body);
+    res.send(req.body);
+
+});
+
+app.use('/', (req, res, next) => {
     console.log("DEFAULT PATH");
     res.send("<html><head><body><h1>I AM NODE JS SERVER WITH EXPRESS JS...path = /</h2></body></head></html>")
     next(); //going to next line of route 
 
 });
+
 app.listen(3000);
